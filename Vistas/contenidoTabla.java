82f814d46875ddb719b11d2fa2e0f55e3e1494d6
JuliRZ <106175091+JuliRZ@users.noreply.github.com>
2022-07-04 23:54:40 -0500

    Add files via upload

---
+++ Vistas/contenidoTabla.java
@@ -0,0 +1,7 @@
+package Vistas;
+class contenidoTabla {
+    static void removeRow(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
