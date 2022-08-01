74026b0c9746790d3a45962ea8662f5e60ccd3a7
JuliRZ <106175091+JuliRZ@users.noreply.github.com>
2022-07-31 23:10:09 -0500

    Delete Vistas directory

--- Vistas/contenidoTabla.java
+++
@@ -1,7 +0,0 @@
-package Vistas;
-class contenidoTabla {
-    static void removeRow(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-}
